d81b5ebb3b9cd199d835de922e41c0fd494972fc
VitorGabrielGomes <gvitor355@gmail.com>
2025-04-30 21:41:29 -0300

    Add files via upload

---
+++ aula2.py
@@ -0,0 +1,13 @@
+#Estudando variáveis:
+
+nome_aluno: str = "Vitor G Gomes"  #Variável do tipo string/texto/alfanumérico  
+idade_professor: int = 15 #Variável do tipo inteiro
+esta_ativo: bool = True #Variável do tipo booleano/ True or False
+nota_aluno: float = 8.75 #Variável do tipo float/ Decimal
+
+print(nome_aluno)
+
+#Cada variável vale por uma linha
+nome_aluno = "Ana Maria"
+print(nome_aluno)
+
