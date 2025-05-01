732aa8249dbf3b2f945995a99dbc449ebb6e1833
VitorGabrielGomes <gvitor355@gmail.com>
2025-04-30 21:56:10 -0300

    Add files via upload

---
+++ aula3.py
@@ -0,0 +1,21 @@
+#Comando de entrada de dados input()
+#Comando de saída de dados via terminal print()
+
+nome_recebido: str = ""
+nome_recebido = input("Digite uma mensagem: ")
+print("O nome é", nome_recebido)
+
+'''
+Operacões matemáticas:
++ soma 
+- subtração
+/ divisão
+* multiplicação
+'''
+
+num1: int = 5 # primeiro valor a cálcular
+num2: int = 10 #segundo valor a cálcular
+total: float = 0 #receber o valor dos cálculos
+
+total = num1 + num2 #realizando uma soma
+print("Resultado da soma é", total )
---
+++ testeaula3matemáticas.py
@@ -0,0 +1,11 @@
+num1: int = 5 # primeiro valor a cálcular
+num2: int = 10 #segundo valor a cálcular
+total1: float = 0 #receber o valor dos cálculos
+total2: float = 0 #receber o valor dos cálculos
+
+
+total1 = num1 + num2 #realizando uma soma
+total2 = total1 - num2
+
+print("Resultado da soma é", total1)
+print("Resultado da soma é", total2)
